14fea2538654f765a4c678b7e34f24e6e4b5eb83
Oleg Zaimkin <oleg.zaimkin@grapecity.com>
2019-02-28 16:12:51 +0700

    a bit of simplification

--- Parser/Program.fs
+++ Parser/Program.fs
@@ -3,75 +3,42 @@
 open System
 open FSharp.Data
 open System.Text.RegularExpressions
-open System
 open System.IO
 
-type Basecamp = JsonProvider<"D:\\Codefest\\todos.json">
+type Basecamp = JsonProvider<"../todos.json">
 
+let grabEmail s =
+    Regex.Matches(s, @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*")
+    |> Seq.cast<Match>
+    |> Seq.map (fun m -> m.Value)
+    |> Seq.tryHead
 
-let grabEmail (s:option<string>) :string =
-    match s with
-      | Some data ->
-                let matches = Regex.Matches(data, @"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*")
-                if Seq.isEmpty matches 
-                then ""
-                else
-                            matches
-                            |> Seq.cast<Match>
-                            |> Seq.map (fun m -> m.Value)
-                            |> Seq.head
-                            
-      | None -> ""
-   
-let grabName (s:string):string =
-    if s.IndexOf("(") > 0 then
-        s.Substring(0, s.IndexOf("(")).Trim();
-    else s
-
-type RecordTest = { Description: string; Title: string }
-
-let readJson :  System.Collections.Generic.List<RecordTest> =
-  let files = Directory.GetFiles "D:\\Codefest\\Data"
-
-  let results = new System.Collections.Generic.List<RecordTest>()
- 
-  for file in files do
-    let content = Basecamp.Load file
-    
-
-    let result = content |> Seq.map(fun issue -> 
-                                 let email = grabEmail issue.Description
-                                 let name = grabName issue.Title
-                                 {Description = email; Title = name}
-                              )
-                              |> Seq.toArray
-    results.AddRange result
-  
-  results
-
+let grabName (s:string) =
+    match s.IndexOf("(") with
+    | p when p > 0 -> s.Substring(0, p).Trim();
+    | _ -> s
 
 [<EntryPoint>]
 let main argv =
 
     let selectedSpeakers = [
-        "Тоболь Александр Александрович"
+        "Тоболь"
         "Панченко Иван Евгеньевич"
-        "Фастов Иван Владимирович"
-        "Чечель Максим Михайлович"
-        "Чапоргин Антон"
-        "Неволин Роман"
-        "Кочепасов Антон Алексеевич"
-        "Абилов Вагиф Галлиевич"
-        "Михаил Ярийчук"
-        "Кирпичников Алексей Николаевич"
-        "Аршинов Максим Вячеславович"
+        "Фастов"
+        "Чечель"
+        "Чапоргин"
+        "Неволин"
+        "Кочепасов"
+        "Абилов Вагиф"
+        "Ярийчук"
+        "Кирпичников"
+        "Аршинов"
         "Dylan Beattie"
-        "Валеев Тагир Фаридович"
-        "Финкельштейн Павел Михайлович"
-        "Александров Дмитрий Вячеславович"
-        "Липский Никита Валерьевич"
-        "Александров Дмитрий Вячеславович"
-        "Кошелев Григорий Николаевич"
+        "Валеев Тагир"
+        "Финкельштейн Павел"
+        "Александров Дмитрий"
+        "Липский Никита"
+        "Кошелев Григорий"
         "Паньгин"
         "Толстопятов"
         "Плизга"
@@ -81,22 +48,18 @@ let main argv =
     // https://3.basecamp.com/3152562/buckets/8281413/todolists/1303394617/todos.json?completed=true&page=2
     // https://3.basecamp.com/3152562/buckets/8281413/todolists/1303394617/groups.json
     // https://github.com/basecamp/bc3-api/blob/master/sections/todolist_groups.md#to-do-list-groups
-    
-    let todos = 
-        //Basecamp.GetSamples()
-        readJson
-        |> Seq.filter (fun issue ->  
-                    let name = issue.Title
-                    List.contains name selectedSpeakers |> not)
-        |> Seq.map(fun issue -> 
-                 let email = issue.Description
-                 let name = issue.Title
-                 name + "," + email
-                 )
-        |> Seq.distinct
 
-    
-    File.AppendAllLines("D:\\Codefest\\decline.csv", todos)
+    let readTodos path =
+      [ for file in Directory.GetFiles path do
+           for issue in Basecamp.Load file do
+               let name = grabName issue.Title
+               if not(selectedSpeakers |> List.exists (name.Contains)) then
+                   let email = grabEmail issue.Description |> Option.defaultValue ""
+                   yield name + "," + email ]
+
+    let todos = readTodos "C:\\Codefest\\Data" |> List.distinct
+
+    File.AppendAllLines("c:\\Codefest\\decline.csv", todos)
     for issue in todos do
         printfn "%s" issue
     Console.ReadLine() |> ignore
